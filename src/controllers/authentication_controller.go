3acd93f9b39ca6631ceb28ead2f74fe0058182b9
Kaung Myat Han <arkarhtethan@gmail.com>
2023-12-25 21:10:24 +0630

    feat(authentication): auth controller setup

---
+++ src/controllers/authentication_controller.go
@@ -0,0 +1,34 @@
+package controllers
+
+import "github.com/gofiber/fiber/v2"
+
+type AuthenticationController struct {
+}
+
+func (controller *AuthenticationController) Login(ctx *fiber.Ctx) error {
+	return ctx.JSON(fiber.Map{"message": "Login route"})
+}
+
+func (controller *AuthenticationController) Logout(ctx *fiber.Ctx) error {
+	return ctx.JSON(fiber.Map{"message": "Logout route"})
+}
+
+func (controller *AuthenticationController) Register(ctx *fiber.Ctx) error {
+	return ctx.JSON(fiber.Map{"message": "Register route"})
+}
+
+func (controller *AuthenticationController) RefreshToken(ctx *fiber.Ctx) error {
+	return ctx.JSON(fiber.Map{"message": "RefreshToken route"})
+}
+
+func (controller *AuthenticationController) ForgotPassword(ctx *fiber.Ctx) error {
+	return ctx.JSON(fiber.Map{"message": "ForgotPassword route"})
+}
+
+func (controller *AuthenticationController) ResetPassword(ctx *fiber.Ctx) error {
+	return ctx.JSON(fiber.Map{"message": "ResetPassword route"})
+}
+
+func (controller *AuthenticationController) VerifyEmail(ctx *fiber.Ctx) error {
+	return ctx.JSON(fiber.Map{"message": "ResetPassword route"})
+}
