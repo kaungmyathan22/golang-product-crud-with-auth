ba1b67321689eefe936fbf090347bc40ef61d793
Kaung Myat Han <arkarhtethan@gmail.com>
2023-12-26 23:32:55 +0630

    feat(user): repo / service / collection setup & pass service to controller

--- src/routes/authentication_routes.go
+++ src/routes/authentication_routes.go
@@ -2,13 +2,27 @@ package routes
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/kaungmyathan22/golang-product-crud-with-auth/src/config"
 	"github.com/kaungmyathan22/golang-product-crud-with-auth/src/controllers"
+	"github.com/kaungmyathan22/golang-product-crud-with-auth/src/repositories"
+	"github.com/kaungmyathan22/golang-product-crud-with-auth/src/services"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func InitAuthenticationRoutes(routeGroup fiber.Router) {
-	authentication_controller := controllers.AuthenticationController{}
+func InitAuthenticationRoutes(routeGroup fiber.Router, client *mongo.Client) {
+	userCollection := client.Database(config.AppConfigInstance.DATABASE_NAME).Collection(config.AppConfigInstance.USER_COLLECTION)
+	userRepository := &repositories.UserRepository{
+		UserCollection: userCollection,
+	}
+	authentication_service := services.UserService{
+		Repository: userRepository,
+	}
+	authentication_controller := controllers.AuthenticationController{
+		Service: &authentication_service,
+	}
 	router := routeGroup.Group("/authentication")
 	router.Post("/login", authentication_controller.Login)
+	router.Post("/register", authentication_controller.Register)
 	router.Post("/logout", authentication_controller.Logout)
 	router.Post("/forgot-password", authentication_controller.ForgotPassword)
 	router.Post("/reset-password", authentication_controller.ResetPassword)
