6c292659eec9098bd5ea0e57fe8212906e6e43a3
Kaung Myat Han <arkarhtethan@gmail.com>
2023-12-28 22:10:15 +0630

    update(dto): user dto added

--- src/dto/user_dto.go
+++ src/dto/user_dto.go
@@ -1,5 +1,32 @@
 package dto
 
+import (
+	"time"
+
+	"github.com/kaungmyathan22/golang-product-crud-with-auth/src/models"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type UserDTO struct {
+	ID         primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	CreatedAt  time.Time          `bson:"created_at" json:"created_at"`
+	UpdatedAt  time.Time          `bson:"updated_at" json:"updated_at"`
+	Username   string             `bson:"username" json:"username"`
+	Password   string             `bson:"password" json:"-"`
+	IsDisabled bool               `bson:"isDisabled" json:"isDisabled"`
+}
+
+func (dto *UserDTO) ToModel() *models.UserModel {
+	return &models.UserModel{
+		ID:         dto.ID,
+		Username:   dto.Username,
+		Password:   dto.Password,
+		CreatedAt:  dto.CreatedAt,
+		UpdatedAt:  dto.UpdatedAt,
+		IsDisabled: dto.IsDisabled,
+	}
+}
+
 type CreateUserDTO struct {
 	Username string `json:"username" validate:"required,min=3" error:"username field is required and must be a minimum of 3."`
 	Password string `json:"password" validate:"required,min=6" error:"password field is required and must be a minimum of 6."`
