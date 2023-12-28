8a9b1b567c252ae35415c9699c2c64f3f016b993
Kaung Myat Han <arkarhtethan@gmail.com>
2023-12-28 22:09:41 +0630

    feat(authentication): user login feat added

--- src/controllers/authentication_controller.go
+++ src/controllers/authentication_controller.go
@@ -5,17 +5,62 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/kaungmyathan22/golang-product-crud-with-auth/src/common"
 	"github.com/kaungmyathan22/golang-product-crud-with-auth/src/dto"
+	"github.com/kaungmyathan22/golang-product-crud-with-auth/src/logger"
 	"github.com/kaungmyathan22/golang-product-crud-with-auth/src/services"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var validate = validator.New(validator.WithRequiredStructEnabled())
 
 type AuthenticationController struct {
-	Service *services.UserService
+	Service      *services.UserService
+	TokenService *services.TokenService
 }
 
 func (controller *AuthenticationController) Login(ctx *fiber.Ctx) error {
-	return ctx.JSON(fiber.Map{"message": "Login route"})
+	var payload dto.LoginDTO
+	if err := ctx.BodyParser(&payload); err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(common.ErrorResponse{
+			Errors: common.TransformError(err.Error()),
+			Code:   fiber.StatusBadRequest,
+		})
+	}
+	if err := validate.Struct(payload); err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(common.ErrorResponse{
+			Code:   fiber.StatusBadRequest,
+			Errors: common.TransformError(err.Error()),
+		})
+	}
+	user, err := controller.Service.GetUserByUsername(payload.Username)
+	if err != nil {
+		if err := ctx.BodyParser(&payload); err != nil {
+			return ctx.Status(fiber.StatusBadRequest).JSON(common.ErrorResponse{
+				Errors: common.TransformError(err.Error()),
+				Code:   fiber.StatusBadRequest,
+			})
+		}
+	}
+	if user == nil {
+		return ctx.Status(fiber.StatusUnauthorized).JSON(common.ErrorResponse{
+			Errors: common.TransformError("invalid username / password."),
+			Code:   fiber.StatusUnauthorized,
+		})
+	}
+	if err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(payload.Password)); err != nil {
+		return ctx.Status(fiber.StatusUnauthorized).JSON(common.ErrorResponse{
+			Errors: common.TransformError("invalid username / password."),
+			Code:   fiber.StatusUnauthorized,
+		})
+	}
+	token, err := controller.TokenService.SignAccessToken(user.ID.Hex())
+	if err != nil {
+		logger.Info(err.Error())
+		return ctx.Status(fiber.StatusInternalServerError).JSON(common.ErrorResponse{
+			Errors: common.TransformError("error signing access token."),
+			Code:   fiber.StatusInternalServerError,
+		})
+	}
+	return ctx.JSON(fiber.Map{"user": user, "token": token})
 }
 
 func (controller *AuthenticationController) Logout(ctx *fiber.Ctx) error {
