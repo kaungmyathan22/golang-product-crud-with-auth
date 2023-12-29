49b3a88c610da05e4d6cd293170d3fcd0ac1a29e
Kaung Myat Han <arkarhtethan@gmail.com>
2023-12-29 21:20:16 +0630

    feat(prdouct): auth required middleware added

--- src/routes/product_route.go
+++ src/routes/product_route.go
@@ -2,12 +2,24 @@ package routes
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/kaungmyathan22/golang-product-crud-with-auth/src/config"
 	"github.com/kaungmyathan22/golang-product-crud-with-auth/src/controllers"
+	"github.com/kaungmyathan22/golang-product-crud-with-auth/src/middlewares"
+	"github.com/kaungmyathan22/golang-product-crud-with-auth/src/repositories"
+	"github.com/kaungmyathan22/golang-product-crud-with-auth/src/services"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func InitProductRoutes(routeGroup fiber.Router) {
+func InitProductRoutes(routeGroup fiber.Router, client *mongo.Client) {
 	product_controller := controllers.ProductController{}
-	router := routeGroup.Group("/products")
+	userCollection := client.Database(config.AppConfigInstance.DATABASE_NAME).Collection(config.AppConfigInstance.USER_COLLECTION)
+	userRepository := &repositories.UserRepository{
+		UserCollection: userCollection,
+	}
+	authentication_service := services.UserService{
+		Repository: userRepository,
+	}
+	router := routeGroup.Group("/products", middlewares.IsAuthenticatedMiddleware(authentication_service))
 	router.Get("/", product_controller.GetProducts)
 	router.Post("/", product_controller.CreateProduct)
 	router.Get("/:id", product_controller.GetProduct)
