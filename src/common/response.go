5493366b9dbc3707d52bd45764705efbdc62cbe6
Kaung Myat Han <arkarhtethan@gmail.com>
2023-12-30 23:42:09 +0630

    update(): pagination struct added

--- src/common/response.go
+++ src/common/response.go
@@ -4,3 +4,7 @@ type ErrorResponse struct {
 	Code   int      `json:"code"`
 	Errors []string `json:"errors"`
 }
+type PaginationParams struct {
+	Page     int `query:"page,default=1"`
+	PageSize int `query:"pageSize,default=10"`
+}
