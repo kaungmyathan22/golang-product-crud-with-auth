b4c12d0bfc616994fdb0f392a88a75def0f4ad6c
Kaung Myat Han <arkarhtethan@gmail.com>
2023-12-30 22:34:59 +0630

    update(): use usermodel added instead of user dto

--- src/services/user_service.go
+++ src/services/user_service.go
@@ -7,7 +7,6 @@ import (
 	"github.com/kaungmyathan22/golang-product-crud-with-auth/src/dto"
 	"github.com/kaungmyathan22/golang-product-crud-with-auth/src/models"
 	"github.com/kaungmyathan22/golang-product-crud-with-auth/src/repositories"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"golang.org/x/crypto/bcrypt"
 )
@@ -16,30 +15,26 @@ type UserService struct {
 	Repository *repositories.UserRepository
 }
 
-func (svc *UserService) CreateUser(payload *dto.CreateUserDTO) (*models.UserModel, error) {
-	//TODO: decrypt password
+func (svc *UserService) CreateUser(payload *dto.CreateUserDTO) (*dto.UserDTO, error) {
 	hashed_password, err := bcrypt.GenerateFromPassword([]byte(payload.Password), bcrypt.DefaultCost) //payload.Password
 	if err != nil {
 		return nil, err
 	}
-
-	user := dto.UserDTO{
+	user := models.UserModel{
 		Username:   payload.Username,
 		Password:   string(hashed_password),
 		CreatedAt:  time.Now(),
 		UpdatedAt:  time.Now(),
 		IsDisabled: false,
 	}
-	result, err := svc.Repository.CreateUser(&user)
+	_, err = svc.Repository.CreateUser(&user)
 	if err != nil {
 		if mongo.IsDuplicateKeyError(err) {
 			return nil, fmt.Errorf("user with the name %s already exists", user.Username)
 		}
 		return nil, err
 	}
-	fmt.Println(result.InsertedID)
-	user.ID = result.InsertedID.(primitive.ObjectID)
-	return user.ToModel(), err
+	return user.ToUserDTO(), nil
 }
 
 func (svc *UserService) GetUserByUsername(username string) (*dto.UserDTO, error) {
