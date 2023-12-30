a0c4077bd62fc189497c0aeace4fbd437b267d81
Kaung Myat Han <arkarhtethan@gmail.com>
2023-12-30 22:32:51 +0630

    feat(user): to user dto utils method added

--- src/models/user_model.go
+++ src/models/user_model.go
@@ -3,6 +3,7 @@ package models
 import (
 	"time"
 
+	"github.com/kaungmyathan22/golang-product-crud-with-auth/src/dto"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
@@ -13,4 +14,15 @@ type UserModel struct {
 	Username   string             `bson:"username"`
 	Password   string             `bson:"password"`
 	IsDisabled bool               `bson:"isDisabled"`
+}
+
+func (model *UserModel) ToUserDTO() *dto.UserDTO {
+	return &dto.UserDTO{
+		ID:         model.ID,
+		CreatedAt:  model.CreatedAt,
+		UpdatedAt:  model.UpdatedAt,
+		Username:   model.Username,
+		Password:   model.Password,
+		IsDisabled: model.IsDisabled,
+	}
 }
