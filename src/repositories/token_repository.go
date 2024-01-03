7fc49d951d466cb5924581eeea374ec6dfbe5d3b
Kaung Myat Han <arkarhtethan@gmail.com>
2024-01-03 23:23:56 +0630

    update(Token): checking existing token issue fixed

--- src/repositories/token_repository.go
+++ src/repositories/token_repository.go
@@ -6,7 +6,6 @@ import (
 	"github.com/kaungmyathan22/golang-product-crud-with-auth/src/dto"
 	"github.com/kaungmyathan22/golang-product-crud-with-auth/src/models"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -15,13 +14,9 @@ type TokenRepository struct {
 }
 
 func (repository *TokenRepository) CreateNewToken(payload *dto.CreateRefreshTokenDTO) error {
-	userId, err := primitive.ObjectIDFromHex(payload.UserID)
-	if err != nil {
-		return err
-	}
-	existingToken := repository.TokenCollection.FindOne(ctx, bson.M{"userId": userId})
-	if existingToken.Err() != nil {
-		if errors.Is(existingToken.Err(), mongo.ErrNoDocuments) {
+	var tokenModel models.RefreshTokenModel
+	if err := repository.TokenCollection.FindOne(ctx, bson.M{"userID": payload.UserID}).Decode(&tokenModel); err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
 			// create new token
 			_, err := repository.TokenCollection.InsertOne(ctx, payload)
 			if err != nil {
@@ -31,12 +26,7 @@ func (repository *TokenRepository) CreateNewToken(payload *dto.CreateRefreshToke
 		}
 		return err
 	}
-	// update existing token
-	var tokenModel models.RefreshTokenModel
-	if err := existingToken.Decode(&tokenModel); err != nil {
-		return err
-	}
-	_, err = repository.TokenCollection.UpdateOne(ctx, bson.M{"_id": tokenModel.ID}, payload)
+	_, err := repository.TokenCollection.UpdateOne(ctx, bson.M{"_id": tokenModel.ID}, bson.M{"$set": payload})
 	if err != nil {
 		return err
 	}
