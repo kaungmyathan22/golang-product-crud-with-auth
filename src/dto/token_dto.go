426c32ab36d2d6dbfb1d7f58c539ea410250aee0
Kaung Myat Han <arkarhtethan@gmail.com>
2024-01-03 23:15:31 +0630

    update(dto): update expiration time data type

--- src/dto/token_dto.go
+++ src/dto/token_dto.go
@@ -1,7 +1,9 @@
 package dto
 
+import "time"
+
 type CreateRefreshTokenDTO struct {
-	TokenHash      string `bson:"tokenHash" json:"tokenHash"`
-	ExpirationTime string `bson:"expirationTime" json:"expirationTime"`
-	UserID         string `bson:"userID" json:"userID"`
+	TokenHash      string    `bson:"tokenHash" json:"tokenHash"`
+	ExpirationTime time.Time `bson:"expirationTime" json:"expirationTime"`
+	UserID         string    `bson:"userID" json:"userID"`
 }
