49b2fa1f6346bbbe4a3ad822a32c3ff987342f66
Kaung Myat Han <arkarhtethan@gmail.com>
2024-01-04 22:29:30 +0630

    update(auth): forgot password dto added

--- src/dto/authentication_dto.go
+++ src/dto/authentication_dto.go
@@ -1,6 +1,22 @@
 package dto
 
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
 type LoginDTO struct {
 	Username string `json:"username" validate:"required,min=3" error:"username field is required and must be a minimum of 3."`
 	Password string `json:"password" validate:"required,min=6" error:"password field is required and must be a minimum of 6."`
+}
+
+type CreatePasswordResetDTO struct {
+	UserId         primitive.ObjectID
+	Code           int
+	ExpirationTime time.Time
+}
+
+type ForgotPasswordDTO struct {
+	Email string `json:"email" validate:"required,email" error:"email field is required and must be a valid email address."`
 }
